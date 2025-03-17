28a1b45de6ecb542f6721655916c78385411a01a
Kinjal <kinjals505@gmail.com>
2025-03-17 18:52:58 +0530

    some proper working

--- Ayurved_Solution/ayurved.py
+++ Ayurved_Solution/ayurved.py
@@ -116,4 +116,4 @@ def usercategory():
 if __name__=="__main__":
     # app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))  # Render assigns a PORT dynamically
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True , host="0.0.0.0", port=port)
--- Ayurved_Solution/sql.py
+++ Ayurved_Solution/sql.py
@@ -18,13 +18,13 @@ def insertrec(t):
     db.commit()
     db.close()
 #edit record
-# def updaterec(t):
-#     db=getconnection()
-#     cr=db.cursor()
-#     sql="UPDATE medicine SET category=?, problem=?, details=?, image=?, link=? WHERE id=?"
-#     cr.execute(sql,t)
-#     db.commit()
-#     db.close()
+def updaterec(t):
+    db=getconnection()
+    cr=db.cursor()
+    sql="UPDATE medicine SET category=?, problem=?, details=?, image=?, link=? WHERE id=?"
+    cr.execute(sql,t[:6])
+    db.commit()
+    db.close()
  
  
 #display record
@@ -43,7 +43,7 @@ def deleterec(id):
     db=getconnection()
     cr=db.cursor()
     sql="delete from medicine where id=?"
-    cr.execute(sql,id)
+    cr.execute(sql,(id,))
     db.commit()
     db.close()
  
